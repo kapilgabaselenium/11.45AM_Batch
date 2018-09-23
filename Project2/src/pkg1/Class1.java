71bf70654e907d2da57a1a3519d7cad6a1331040
Dell <Dell@DESKTOP-30FNJH5>
2018-09-22 23:51:51 -0700

    I want to push my code

---
+++ Project2/src/pkg1/Class1.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
+
+public class Class1 {
+	
+	@Test
+	public void tc()
+	{
+		System.out.println("Class1");
+	}
+	
+}
---
+++ Project2/src/pkg1/Class2.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+
+	@Test
+	public void tc()
+	{
+		System.out.println("Class2");
+	}
+	
+}
